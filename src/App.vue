0dc8af6a49d62096ee0fabf10ffa901a98de4cb4
yaozl <yaozl@3vjia.com>
2018-08-22 17:32:56 +0800

    first commit init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <waveAnimation></waveAnimation>
+  </div>
+</template>
+
+<script>
+import waveAnimation from './components'
+export default {
+  name: 'app',
+  components: {
+    waveAnimation
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/WaveAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: "WaveAnimation",
+    data() {
+      return {
+        msg: 'this is waveAnimation'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
